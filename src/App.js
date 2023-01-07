4ed8e1f9293e49a229f51617d94fb4b4f9bb4b0a
valeriacamarillo <valeria1213@gmail.com>
2023-01-06 21:16:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
+
+const cardImages = [
+  {"src": "/img/closing.png", matched: false},
+  {"src": "/img/html.png", matched: false},
+  {"src": "/img/java.png", matched: false},
+  {"src": "/img/js.png", matched: false},
+  {"src": "/img/python.png", matched: false},
+  {"src": "/img/react.png", matched: false}
+
+]
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+
+  //shuffle cards 
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.random()}))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+     
+  }
+
+  //handle choice 
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  //compare 2 selected cards 
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000) 
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  //start a new game automatically//
+  useEffect(() => {
+    shuffleCards()
+  }, [] )
+
+  return (
+    <div className="App">
+      <h1>Code Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+       <SingleCard 
+       key={card.id} 
+       card={card}
+       handleChoice={handleChoice}
+       flipped={card === choiceOne || card === choiceTwo || card.matched}
+       disabled={disabled}
+       />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App
